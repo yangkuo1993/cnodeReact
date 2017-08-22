cd4e7cf010f9c1226f44c54e7a3c823e79950473
杨阔 <734271234@qq.com>
2017-08-22 22:04:18 +0800

    路由测试

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './views/App/App';
+import AuthExample from './views/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<AuthExample />, document.getElementById('root'));
 registerServiceWorker();
--- src/views/App/App.js
+++ src/views/App/App.js
@@ -1,21 +1,157 @@
 import React, { Component } from 'react';
-import logo from '../../assets/images/logo.svg';
+// import logo from '../../assets/images/logo.svg';
 import './App.css';
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect,
+  withRouter
+} from 'react-router-dom'
+// const BasicExample = () => (
+//   <Router>
+//     <div>
+//       <ul>
+//         <li><Link to="/">Home</Link></li>
+//         <li><Link to="/about">About</Link></li>
+//         <li><Link to="/topics">Topics</Link></li>
+//       </ul>
+//       <hr/>
+//       <Route exact path="/" component={App}/>
+//       <Route path="/about" component={About}/>
+//       <Route path="/topics" component={Topics}/>
+//     </div>
+//   </Router>
+// )
+// const About = () => (
+//   <div>
+//     <h6>About</h6>
+//   </div>
+// )
+// const Topics = ({match}) => (
+//   <div>
+//     <h2>二级路由界面</h2>
+//     <ul>
+//       <li>
+//         <Link to={`${match.url}/rendering`}>
+//           Readering with React
+//         </Link>
+//       </li>
+//       <li>
+//         <Link to={`${match.url}/components`}>
+//           Components
+//         </Link>
+//       </li>
+//       <li>
+//         <Link to={`${match.url}/props-v-state`}>
+//           Props v. State
+//         </Link>
+//       </li>
+//     </ul>
+//     <Route path={`${match.url}/:topicId`} component={Topic}/>
+//     <Route exact path={match.url} render={() => (
+//       <h3>Please select a topic.</h3>
+//     )}/>
+//   </div>
+// )
+// const Topic = ({ match }) => (
+//   <div>
+//     <h3>{match.params.topicId}</h3>
+//   </div>
+// )
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <div className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <h2>Welcome to React</h2>
+//         </div>
+//         <p className="App-intro">
+//           To get started, edit <code>src/App.js</code> and save to reload.
+//         </p>
+//       </div>
+//     );
+//   }
+// }
+const AuthExample = () => (
+  <Router>
+    <div>
+      <AuthButton/>
+      <ul>
+        <li><Link to="/public">Public Page</Link></li>
+        <li><Link to="/protected">Protected Page</Link></li>
+      </ul>
+      <Route path="/public" component={Public}/>
+      <Route path="/login" component={Login}/>
+      <PrivateRoute path="/protected" component={Protected}/>
+    </div>
+  </Router>
+)
+const fakeAuth = {
+  isAuthenticated: false,
+  authenticate(cb) {
+    this.isAuthenticated = true
+    setTimeout(cb, 100) // fake async
+  },
+  signout(cb) {
+    this.isAuthenticated = false
+    setTimeout(cb, 100)
   }
 }
+const AuthButton = withRouter(({ history }) => (
+  fakeAuth.isAuthenticated ? (
+    <p>
+      Welcome! <button onClick={() => {
+      fakeAuth.signout(() => history.push('/'))
+    }}>Sign out</button>
+    </p>
+  ) : (
+    <p>You are not logged in.</p>
+  )
+))
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route {...rest} render={props => (
+    fakeAuth.isAuthenticated ? (
+      <Component {...props}/>
+    ) : (
+      <Redirect to={{
+        pathname: '/login',
+        state: { from: props.location }
+      }}/>
+    )
+  )}/>
+)
+const Public = () => <h3>Public</h3>
+const Protected = () => <h3>Protected</h3>
 
-export default App;
+class Login extends Component {
+  state = {
+    redirectToReferrer: false
+  }
+
+  login = () => {
+    fakeAuth.authenticate(() => {
+      this.setState({ redirectToReferrer: true })
+    })
+  }
+
+  render() {
+    const { from } = this.props.location.state || { from: { pathname: '/' } }
+    const { redirectToReferrer } = this.state
+
+    if (redirectToReferrer) {
+      return (
+        <Redirect to={from}/>
+      )
+    }
+
+    return (
+      <div>
+        <p>You must log in to view the page at {from.pathname}</p>
+        <button onClick={this.login}>Log in</button>
+      </div>
+    )
+  }
+}
+export default AuthExample;
